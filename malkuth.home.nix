14a3e675866f0474644861dccce09243765bc683
aitouka21 <wscheungae@gmail.com>
2026-04-06 14:43:57 +0800

    init.

---
+++ malkuth.home.nix
@@ -0,0 +1,11 @@
+{ pkgs, ... }: {
+  home.username = "malkuth";
+  home.homeDirectory = "/home/malkuth";
+  home.stateVersion = "25.11";
+
+  home.packages = with pkgs; [
+    htop
+  ];
+
+  programs.bash.enable = true;
+}
